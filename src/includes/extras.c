c1540a6190241820c5a265a48052be6570623d5e
Yuu528 <yuu528mail@gmail.com>
2024-07-05 19:16:32 +0900

    Fix tag counter

--- src/includes/extras.c
+++ src/includes/extras.c
@@ -15,12 +15,16 @@ void print_tag_length_csv(char *filename) {
   char *ptok, *pnext;
 
   while (fgets(buf, FILE_BUFFER_SIZE, fp) != NULL) {
-    /* Print id */
     ptok = strtok_r(buf, CSV_DELIM, &pnext);
-    printf("%s" CSV_DELIM, ptok);
 
-    /* Print tag, length, remaining */
-    ptok = strtok_r(NULL, CSV_DELIM, &pnext);
-    printf("%s" CSV_DELIM "%zu" CSV_DELIM "%s", ptok, strlen(ptok), pnext);
+    /* Check if tag is not empty */
+    if (*pnext != ',') {
+      /* Print id */
+      printf("%s" CSV_DELIM, ptok);
+
+      /* Print tag, length, remaining */
+      ptok = strtok_r(NULL, CSV_DELIM, &pnext);
+      printf("%s" CSV_DELIM "%zu" CSV_DELIM "%s", ptok, strlen(ptok), pnext);
+    }
   }
 }
